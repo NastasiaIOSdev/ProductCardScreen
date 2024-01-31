d7a00f545ab97236bfa46c92c797cb8f62f72204
AnastasiiaLarina <laranweb@gmail.com>
2024-01-31 20:18:09 +0500

    Setup extension Color, added pictures

---
+++ ProductCardApp/ProductCardApp/Extensions/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  ProductCardApp
+//
+//  Created by Анастасия Ларина on 31.01.2024.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    public static let specialGreen: Color = Color(UIColor(red: 94/255, green: 205/255, blue: 123/255, alpha: 1))
+}
