9729482e7c55f0b10fa79677933b9f9d0b6fca6b
AnastasiiaLarina <laranweb@gmail.com>
2024-01-31 20:10:28 +0500

    Create project

---
+++ ProductCardApp/ProductCardApp/ProductCardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCardAppApp.swift
+//  ProductCardApp
+//
+//  Created by Анастасия Ларина on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProductCardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
