450cea980ca1667a164ee77fd665555d3f154989
AnastasiiaLarina <laranweb@gmail.com>
2024-01-31 22:52:48 +0500

    Setup: QuantityButtonView, PriceButtonView

---
+++ ProductCardApp/ProductCardApp/Core/View/ProductCard/Components/PriceButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  PriceButtonView.swift
+//  ProductCardApp
+//
+//  Created by Анастасия Ларина on 31.01.2024.
+//
+
+import SwiftUI
+
+struct PriceButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PriceButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        PriceButtonView()
+    }
+}
---
+++ ProductCardApp/ProductCardApp/Core/View/ProductCard/Components/QuantityButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  QuantityButtonView.swift
+//  ProductCardApp
+//
+//  Created by Анастасия Ларина on 31.01.2024.
+//
+
+import SwiftUI
+
+struct QuantityButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QuantityButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuantityButtonView()
+    }
+}
